44801f34fbcef65e717a164188a7478d7381aee0
tonTed <yddetco@gmail.com>
2024-08-05 06:40:02 -0400

    Add new page and migrate main in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import TitlePage from "@/components/ui/title-page";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center min-w-screen">
+    <>
       <TitlePage
         title="Bienvenue sur mon portfolio"
         subtitle="Découvrez mes projets et compétences"
       />
-    </main>
+    </>
   );
 }
---
+++ src/app/smart-speech-transcriber/page.tsx
@@ -0,0 +1,14 @@
+import TitlePage from "@/components/ui/title-page";
+
+function SmartSpeechTranscriber() {
+  return (
+    <>
+      <TitlePage
+        title="Smart Speech Transcriber"
+        subtitle="Transcribe your speech to text using AI"
+      />
+    </>
+  );
+}
+
+export default SmartSpeechTranscriber;
