67c4b7c750b2831fa01378f6eb588f71a2ccc4a6
tonTed <yddetco@gmail.com>
2024-08-04 20:04:34 -0400

    Add toggle button to change theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useTheme } from "@/contexts/ThemeContext";
+import { MoonIcon } from "@radix-ui/react-icons";
+
 export default function Home() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <main className="flex flex-col min-h-screen min-w-screen">
-      <h1 className="text-4xl font-bold p-1">tonTed dev</h1>
+      <div className="flex flex-row items-center justify-between p-4">
+        <h1 className="text-4xl font-bold">tonTed dev</h1>
+        <Button variant="ghost" size="icon">
+          <MoonIcon className="h-6 w-6" onClick={() => toggleTheme()} />
+        </Button>
+      </div>
     </main>
   );
 }
